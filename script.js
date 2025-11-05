3a1c962458e81442621b3e932081df6f76bc74fd
devalex <alexalmeidasantos1992@gmail.com>
2025-11-04 23:52:03 -0300

    site ponte rolante

---
+++ script.js
@@ -0,0 +1,34 @@
+AOS.init();
+
+var swiper = new Swiper(".swiper", {
+  loop: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  autoplay: {
+    delay: 5000,
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 18
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 18
+    },
+    // when window width is >= 640px
+    1188: {
+      slidesPerView: 3,
+      spaceBetween: 24
+    }
+  }
+});
